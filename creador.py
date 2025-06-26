532500b34363db44ebada74c787296324d688f01
Agustin Azar <agustin.azar101@gmail.com>
2025-06-26 12:33:18 -0300

    add: crear varias instancias y probar

---
+++ creador.py
@@ -0,0 +1,49 @@
+from random import sample, randint, random
+import math
+from itertools import product
+
+
+def distancia_eucleideana(x, y):
+    return int(math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2))
+
+
+def crear_instancia(costo_max_camion=None, n=None, cant_exclusivos=None,
+                    cant_refrigerados=None, dist_max=None, costo_repartidor=None):
+    ids = [str(i) for i in range(1, n + 1)]
+    # Primeras lineas de la entrada
+    file = open(f"instancias/instancia_{n}.txt", "w")
+    file.write(f"{n}\n")
+    file.write(f"{costo_repartidor}\n")
+    file.write(f"{dist_max}\n")
+    file.write(f"{cant_refrigerados}\n")
+    file.write("\n".join(sample(ids, cant_refrigerados)) + "\n")
+    file.write(f"{cant_exclusivos}\n")
+    file.write("\n".join(sample(ids, cant_exclusivos)) + "\n")
+    # Generar puntos aleatorios en una grilla
+    alto, ancho = 5 * n, 5 * n
+    grilla = [(i, j) for i, j in product(range(alto), range(ancho))]
+    puntos = sample(grilla, n)
+    # Lineas con los puntos, sus distancias y costos
+    for i, c_1 in enumerate(puntos):
+        for j, c_2 in enumerate(puntos):
+            if j <= i: continue
+            dist = distancia_eucleideana(c_1, c_2)
+            costo = randint(1, costo_max_camion)
+            file.write(f"{i} {j} {dist} {costo}\n")
+    file.close()
+
+
+def main():
+    n = 70
+
+    for n in [40, 45, 50, 55, 60, 65, 70]:
+        costo_max_camion = 100
+        costo_repartidor = randint(10, 30)
+        dist_max = 80
+        cant_refrigerados = 5
+        cant_exclusivos = randint(1,int(math.sqrt(n)))
+        crear_instancia(costo_max_camion, n, cant_refrigerados, cant_exclusivos, dist_max, costo_repartidor)
+
+
+if __name__ == '__main__':
+    main()
--- probadorDeModelos.py
+++ probadorDeModelos.py
@@ -10,10 +10,11 @@ def main():
     # Crear carpteta de resultados si no existe
     if not os.path.exists('resultados'):
         os.makedirs('resultados')
-
-    os.system('python nuevoModelo.py instancias/instancia.txt 1 0 > instancias/outputNuevoModelo_default.txt')
-    # os.system('python nuevoModelo.py instancias/instancia.txt 0 0 > instancias/outputNuevoModelo_dfs.txt')
-    # os.system('python nuevoModelo.py instancias/instancia.txt 0 2 > instancias/outputNuevoModelo_dfs_raizA.txt')
+    for instancia in os.listdir('instancias'):
+        instancia_size = instancia.split('.')[0].split('_')[1]
+        os.system(f'python nuevoModelo.py instancias/{instancia} 1 0 > resultados/outputNuevoModelo_{instancia_size}_default.txt')
+        os.system(f'python nuevoModelo.py instancias/{instancia} 0 0 > resultados/outputNuevoModelo_{instancia_size}_dfs.txt')
+        os.system(f'python nuevoModelo.py instancias/{instancia} 0 2 > resultados/outputNuevoModelo_{instancia_size}_dfs_raizA.txt')
 
 if __name__ == '__main__':
     main()
